ff93577f138fb808a896021b68535b17a0678c56
Jamezfn <jamesmakachia19@gmail.com>
2026-04-30 08:31:55 +0300

    Banner component

---
+++ react/landing-page/src/components/banner/Banner.jsx
@@ -0,0 +1,34 @@
+import './banner.css';
+import ellipse from '../../assets/images/ellipse.png';
+import doctor from '../../assets/images/banner-doctor.png'
+
+function Banner() {
+    return (
+        <div className='banner-container'>
+            <div className="banner-content">
+                <div className="banner-heading">
+                    <h2>Everything good starts with good health</h2>
+                </div>
+
+                <div className="banner-sub-heading">
+                    We are here to serve serve people with patient centered-care to deliver outstanding healthcare for better lives.
+                </div>
+
+                <div className="banner-buttons">
+                    <button className='banner-appointment-button'>
+                        Request appointment
+                    </button>
+                    <button className='banner-learn-button'>Learn More</button>
+                </div>
+            </div>
+
+            <div className="banner-graphics">
+                <img src={ ellipse } alt="ellipse" />
+                <img src={ doctor } alt="doctor" />
+            </div>
+
+        </div>
+    );
+}
+
+export default Banner;
